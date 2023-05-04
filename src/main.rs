0d0031e9103f87f5f0011df3102886b0281370d5
Fiddler25 <rager.mka.ti@icloud.com>
2023-05-04 20:13:29 +0900

    ファイル分割

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
-use actix_web::{post, web, App, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
+mod controller;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Product {
-    name: String,
-    description: String,
-    price: f64,
-    stock: u8,
-}
-
-#[derive(Serialize)]
-struct ResponseObj {
-    status: String,
-    message: String,
-    product: Product,
-}
-
-#[post("/v1/products")]
-async fn create_product(product: web::Json<Product>) -> Result<impl Responder, actix_web::Error> {
-    let response = ResponseObj {
-        status: "success".to_string(),
-        message: "product created".to_string(),
-        product: product.into_inner(),
-    };
-    Ok(web::Json(response))
-}
+use actix_web::{web, App, HttpServer};
+use controller::product::create_product;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(create_product))
+    HttpServer::new(|| App::new().route("/v1/products", web::post().to(create_product)))
         .bind("127.0.0.1:8080")?
         .run()
         .await
